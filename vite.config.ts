07c024a1fd1ef7ec69b89ec4273510b20109d0d7
vitalitd <Duncan_Farquhar@hotmail.com>
2026-06-22 18:01:51 +1000

    minimal

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { startVitePlugin } from "@tanstack/start-vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    startVitePlugin({
-      ssr: {
-        target: "cloudflare"
-      }
-    })
-  ],
+  plugins: [tsconfigPaths()],
   ssr: {
     target: "webworker",
     noExternal: true
