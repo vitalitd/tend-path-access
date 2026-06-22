5c181c6b62cbf0c928998b320576c72fbfbf3a5a
vitalitd <Duncan_Farquhar@hotmail.com>
2026-06-22 16:52:21 +1000

    new wrapper

--- worker.js
+++ worker.js
@@ -1,9 +1,17 @@
-import handler from "./dist/server/server.js"
+import * as server from "./dist/server/server.js"
 
 export default {
   async fetch(request, env, ctx) {
-    return handler.fetch
-      ? handler.fetch(request, env, ctx)
-      : handler(request, env, ctx)
+    // TanStack Start SSR exports a default handler function
+    if (typeof server.default === "function") {
+      return server.default(request, env, ctx)
+    }
+
+    // Or it may export a fetch function
+    if (typeof server.fetch === "function") {
+      return server.fetch(request, env, ctx)
+    }
+
+    return new Response("No valid handler found in server.js", { status: 500 })
   }
 }
