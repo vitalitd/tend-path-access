8a60b15861d6b8a68b1322392e1055016609548c
vitalitd <Duncan_Farquhar@hotmail.com>
2026-07-17 11:28:17 +1000

    define route

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,4 +1,10 @@
-export default function Contact() {
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/contact')({
+  component: Contact,
+})
+
+function Contact() {
   return (
     <div className="max-w-xl mx-auto py-12">
       <h1 className="text-3xl font-bold mb-4">Contact</h1>
@@ -6,5 +12,5 @@ export default function Contact() {
       <p className="font-medium">Email: duncan.farquhar@gmail.com</p>
       <p className="font-medium">Phone: 0438 585 148</p>
     </div>
-  );
+  )
 }
